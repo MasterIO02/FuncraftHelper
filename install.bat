0168891057319541311e33238e4d8a691fdc2717
MasterIO02 <40836390+MasterIO02@users.noreply.github.com>
2020-07-14 14:45:28 +0200

    Pour installer et lancer FH sous Windows

---
+++ install.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call npm install node-fetch
+call npm install puppeteer
+call npm install readline
+
+echo Fin de l'installation des prérequis
+pause
---
+++ start.bat
@@ -0,0 +1,3 @@
+@echo off
+node ./funcrafthelper.js
+pause
