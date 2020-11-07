2518e504ce0e1470bff084f29a9f1578ef96d03b
MasterIO02 <40836390+MasterIO02@users.noreply.github.com>
2020-11-07 14:09:58 +0100

    Update 2.1.3

--- FuncraftHelper.js
+++ FuncraftHelper.js
@@ -3,7 +3,8 @@ const { config } = require('process')
 const { app, BrowserWindow } = electron
 const Store = require('electron-store');
 const store = new Store();
-const os = require('os')
+const os = require('os');
+const { NONAME } = require('dns');
 
 // Déclaration des fenêtres
 app.on('ready', function () {
@@ -16,7 +17,6 @@ app.on('ready', function () {
             enableRemoteModule: true
         },
         show: false,
-
     })
     mainWindow.setResizable(false)
     mainWindow.loadURL(`file://${__dirname}/mainWindow.html`)
@@ -35,7 +35,7 @@ if (store.get('firstLaunch') == undefined) {
     store.set('headless', true)
     store.set('username', "*")
     store.set('manualEnter', true)
-    store.set('theme', 'default')
+    store.set('theme', 'dark')
     if (store.get('systemType') == "linux") {
         store.set("logFileLocation", "/home/NOM D'UTILISATEUR/.local/share/az-client/logs/latest.log")
         store.set("chromeLocation", "/usr/bin/google-chrome")
@@ -43,15 +43,26 @@ if (store.get('firstLaunch') == undefined) {
         store.set("logFileLocation", "C:/Users/NOM D'UTILISATEUR/AppData/Roaming/.az-client/logs/latest.log")
         store.set("chromeLocation", "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe")
     }
+    store.set('modeNumber', 1)
+    store.set('pauseEngine', false)
+    store.set('adblocker', false)
 }
 
 // Init new configs si ancienne version
-if(store.get('theme') == undefined) {
+if (store.get('theme') == undefined) {
     store.set('theme', 'default')
 }
-if(store.get('manualEnter') == undefined) {
+if (store.get('manualEnter') == undefined) {
     store.set('manualEnter', true)
 }
-
+if (store.get('modeNumber') == undefined) {
+    store.set('modeNumber', 1)
+}
+if (store.get('pauseEngine') == undefined) {
+    store.set('pauseEngine', false)
+}
+if (store.get('adblocker') == undefined) {
+    store.set('adblocker', false)
+}
 
 
